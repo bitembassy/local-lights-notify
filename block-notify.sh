83a7c467a14b7fb984b8d276e09dce5c995dc0fe
sha-265 <4103710+sha-265@users.noreply.github.com>
2021-11-11 19:13:11 +0200

    Add stuff

--- block-notify.sh
+++ block-notify.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-/bin/echo -e "$(cat allgreen.echo)" | socat - udp-datagram:192.168.1.255:56700,broadcast 
-sleep 1 
-/bin/echo -e "$(cat allwhite.echo)" | socat - udp-datagram:192.168.1.255:56700,broadcast 
+/bin/echo -e "$(cat allgreen.echo)" | socat - udp-datagram:192.168.1.255:56700,broadcast
+notify-send "New block has been mined!" "$1" -t 5000
+/bin/echo -e "$(cat allgreen.echo)" | socat - udp-datagram:192.168.1.255:56700,broadcast
+
+sleep 1
+
+/bin/echo -e "$(cat allwhite.echo)" | socat - udp-datagram:192.168.1.255:56700,broadcast
+/bin/echo -e "$(cat allwhite.echo)" | socat - udp-datagram:192.168.1.255:56700,broadcast
